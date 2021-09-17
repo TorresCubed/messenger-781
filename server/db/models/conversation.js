0bc369e37c729473aa6890340664205427f217d7
Alexis Torres <torres.alexis04@yahoo.com>
2021-09-17 01:25:28 -0700

    Update conversation.js
    Add title to convos

--- server/db/models/conversation.js
+++ server/db/models/conversation.js
@@ -1,6 +1,11 @@
+const { Sequelize } = require("sequelize/types");
 const db = require("../db");
 
-const Conversation = db.define("conversation", {});
+const Conversation = db.define("conversation", {
+  title: {
+    type: Sequelize.STRING
+  }
+});
 
 // find conversation given two user Ids
 
